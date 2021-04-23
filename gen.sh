3b9e61958c90cf281a0dbf8b3bca196b4c6743da
tiaotiao11-22 <724791093@qq.com>
2021-04-23 16:51:44 +0800

    message

--- gen.sh
+++ gen.sh
@@ -19,6 +19,6 @@ do
 
 	echo $curr_img
 	echo $curr_depth
-done 
 
-#srun -A test -J Dep -N 1 --ntasks-per-node=1 --cpus-per-task=8 --gres=gpu:1 -p gpu -t 0-01:00:00 python run.py -i /public/data1/users/zhaiwei16/GitCode/DepthGen/images_ori/beat/drum -o /public/data1/users/zhaiwei16/GitCode/DepthGen/depth/beat/drum
+	srun -A test -J Dep -N 1 --ntasks-per-node=1 --cpus-per-task=8 --gres=gpu:1 -p gpu -t 0-01:00:00 python run.py -i $curr_img -o $curr_depth
+done 
